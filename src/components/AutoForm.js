15106d1eaff9b6a9a7db66fcf137964a813a2524
Ken <kenlane33@gmail.com>
2020-11-09 17:45:37 -0500

    Indent repair.

--- src/components/AutoForm.js
+++ src/components/AutoForm.js
@@ -17,7 +17,6 @@ export const AutoForm = ({ formBind, inputBinds, fields, SubmitBtn }) => {
   return (
     <form {...formBind}>
       {ensureArr(fields, Object.keys(inputBinds)).map( k => {
-        //console.log(k)
         return <DefLabeledValue key={k} label={k+': '} {...inputBinds[k]} />;
       })}
       {SubmitBtn ? <SubmitBtn onClick={formBind.onSubmit}/> : <input type="submit" value="Submit" />}
@@ -26,41 +25,52 @@ export const AutoForm = ({ formBind, inputBinds, fields, SubmitBtn }) => {
 }
 //====###############==================================O
 const DefIndentSpacer = () => <div style={{display:'inline', marginLeft:20}}></div>
+const DefIndentedParent = ({label}) => <div style={{display:'inline'}}>{label}</div>
 
 //===========############==============================O
 export const AutoFormTree = (props) => {
 
-  const { formBind, inputBinds, fields, SubmitBtn, 
+  const { formBind, inputBinds,
+    SubmitBtn, 
+    fields=Object.keys(inputBinds), 
     IndentSpacer=DefIndentSpacer,
     LabeledValue=DefLabeledValue,
+    IndentedParent=DefIndentedParent
   } = props
 
   let drawnParents = []
   
+  const someIndentSpacers = (n) => (n > 0) && Array(n).fill().map((x, i) => <IndentSpacer key={i} />)
+
   return (
     <form {...formBind}>
-      {ensureArr(fields, Object.keys(inputBinds)).map( k => {
+      {fields.map( k => {
         const keyParts = k.split('.')
         const lastK = keyParts[keyParts.length - 1]
-        //const deltaDepth = depth - lastDepth
         const parents = keyParts.slice(0,-1)
         let depth = 0
-        const inParents = (x) => {const ret = drawnParents.includes(x); ret || drawnParents.push(x); return ret}
+        const alreadyDrawn = (x) => {
+          const ret = drawnParents.includes(x)
+          ret || drawnParents.push(x)
+          return ret
+        }
 
-        //console.log('k=',k,' depth=', depth, ' keyParts=', keyParts, ' parents=', parents, ' drawnParents=', drawnParents)
-        return (<div key={k}>
-          {parents.map((x,i)=> {
-            const ret = (inParents(x) ||  
-            <div key={x}>
-              {(depth > 0) && Array(depth).fill().map((x, i) => <IndentSpacer key={i} />)}
-              {titleCase(x)}
-            </div>)
-            depth++;
-            return ret
-          })}
-          {parents.map((x,i)=><IndentSpacer key={i}/>)}
-          <LabeledValue key={k} label={titleCase(lastK)+': '} {...inputBinds[k]} />
-        </div>)
+        return (
+          <div key={k}>
+            {parents.map((x,i)=> {
+              const ret = ( alreadyDrawn(x) ||   // if already drawn, skip drawing it ( thanks to || )
+                <div key={x}>
+                  {someIndentSpacers(depth)}
+                  <IndentedParent label={titleCase(x)} />
+                </div>
+              )
+              depth++;
+              return ret
+            })}
+            {parents.map((x,i)=><IndentSpacer key={i}/>)}
+            <LabeledValue key={k} label={titleCase(lastK)+': '} {...inputBinds[k]} />
+          </div>
+        )
       })}
       {SubmitBtn ? <SubmitBtn onClick={formBind.onSubmit}/> : <input type="submit" value="Submit" />}
     </form>
